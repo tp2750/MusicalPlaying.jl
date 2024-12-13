166a98b949339f4d366dda9839c5c144f1dab858
Thomas Poulsen <ta.poulsen@gmail.com>
2024-12-13 23:11:06 +0100

    first working version

--- src/MusicalPlaying.jl
+++ src/MusicalPlaying.jl
@@ -1,5 +1,9 @@
 module MusicalPlaying
 
-# Write your package code here.
+import MIDI
+import WAV
+
+include("notes.jl")
+export Note, note, play_wav_direct, Chord, chord
 
 end
---
+++ src/notes.jl
@@ -0,0 +1,70 @@
+import MIDI
+import WAV
+
+"""
+        AbstractNote covers Note and Chord
+"""
+abstract type AbstractNote end
+
+"""
+        `Note(pitch::Int, duration::Float32, volume::Float32)`
+        `pitch` is the pitch number. MIDI sets pitch 69 to A4 and 440 Hz. This can be modulated by a Tuning.
+        `loudness` is relative volume. A note of loudness 2 has twice the amplitued as one of loudness 1.
+        `duration` is the duration in beats. Typically a quater note has a duration of 1, and a half note a duration of 2.
+"""
+struct Note
+    pitch::Int
+    loudness::Float32
+    duration::Float32
+end
+note(n::Int; l=1, d=1) = Note(n,l,d)
+note(n::T; l=1, d=1) where T <: AbstractString = Note(MIDI.name_to_pitch(n), l, d)
+
+function play_wav_direct(n; bpm=60, samplerate = 44100, file=tempname())
+    save_wav_direct(n; bpm, samplerate, file)
+    WAV.wavplay(file)
+end
+
+function save_wav_direct(n::Note; bpm=60, samplerate = 44100, file=tempname())
+    y = sample_wav_direct(n; bpm, samplerate)
+    WAV.wavwrite(y, file, Fs=samplerate)
+    file
+end
+
+function save_wav_direct(c::Chord; bpm=60, samplerate = 44100, file=tempname())
+    ns = map(x -> note(x[1], l = x[2], d = c.duration), zip(c.pitch, c.loudness))
+    ys = sample_wav_direct.(ns; bpm, samplerate)
+    y = sum(ys)
+    WAV.wavwrite(y, file, Fs=samplerate)
+    file
+end
+
+function save_wav_direct(v::Vector{AbstractNote}; bpm=60, samplerate = 44100, file=tempname())
+
+end
+
+save_wav_direct(s::String; bpm=60, samplerate = 44100, file=tempname()) = save_wav_direct(note(s); bpm, samplerate, file)
+
+function sample_wav_direct(n::Note; bpm=60, samplerate = 44100)
+    freq = MIDI.pitch_to_hz(n.pitch)
+    x = 0:1/samplerate:prevfloat(n.duration*60/bpm)
+    y = n.loudness * sin.(2pi * freq * x )
+    y
+end
+
+struct Chord ## TODO @assert length(pitch) == length(loudness)
+    pitch::Vector{Int}
+    loudness::Vector{Float32}
+    duration::Float32
+    # @assert length(pitch) == length(loudness)
+    # new(pitch, loudness, duration)
+end
+
+function chord(ns::Vector{Note})
+    Chord(map(x -> x.pitch, ns),
+          map(x -> x.loudness, ns),
+          only(unique(map(x -> x.duration, ns)))
+          )
+end
+
+chord(ns::Vector{String}; l = 1, d = 1) = chord(note.(ns; l, d))
